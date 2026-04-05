41cd43b93af6f481782e9969ed1bae8d55f1ebe6
n <norman.koch@tu-dresden.de>
2026-04-06 00:34:20 +0200

    fix

--- app.py
+++ app.py
@@ -948,7 +948,6 @@ canvas{background:#0d1117}
 <option value="EleutherAI/pythia-2.8b">Pythia 2.8B</option>
 <option value="facebook/opt-1.3b">OPT 1.3B</option>
 <option value="microsoft/phi-2">Phi-2 (2.7B)</option>
-<option value="microsoft/deberta-v3-large">DeBERTa-v3 Large</option>
 </select>
 </div>
 <div id="text-area">
